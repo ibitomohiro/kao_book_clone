13928b810fc6cc9860c12511a461e8696fb40822
tomohiro ibi <68018348+ibitomohiro@users.noreply.github.com>
2020-09-22 16:30:07 +0900

    jquery

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'posts#home'
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show]
+  resources :users
   resources :posts
 end
