30160dc250e9963f46880a3897c1611e28a489dc
tomohiro ibi <68018348+ibitomohiro@users.noreply.github.com>
2020-09-24 21:12:00 +0900

    define current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
   def logged_in_user
     unless logged_in?
       flash[:danger] = "Please log in"
-      redirect_to new_sessions_path
+      redirect_to new_session_path
     end
   end
 
